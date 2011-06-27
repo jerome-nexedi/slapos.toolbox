d817f62c7ce393f56f69bd7d877511b6bb0f5e82
Łukasz Nowak <luke@nexedi.com>
2011-06-27 10:05:21 +0200

    Use slapos provided networkcached buildout.
    As older versions of slapos.core will ignore slapos:buildout switch it is safe
    to direct them to unexisting one.
    
    New slapos profiles are generating buildout_networkache in self location, so as
    already all parameters are hardocded, use such path directly.

--- slapos/builder/__init__.py
+++ slapos/builder/__init__.py
@@ -177,7 +177,9 @@ def run(config):
             'template/slapos.cfg.in').read() % dict(
               computer_id=config.computer_id, master_url=config.master_url,
               key_file=key_file, cert_file=cert_file,
-              certificate_repository_path=certificate_repository_path
+              certificate_repository_path=certificate_repository_path,
+              buildout=os.path.join('/opt', 'slapos', 'bin',
+                'buildout_networkache')
             ))
 
       hostname_path = os.path.normpath('/'.join([mount_dir_path,
