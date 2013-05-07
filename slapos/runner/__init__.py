e91dfff1fc13ea311cc45b0096e510fc0a39301b
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-05-07 21:13:25 +0200

    Add logger when starting to state that slapgrid is running.

--- slapos/runner/__init__.py
+++ slapos/runner/__init__.py
@@ -145,6 +145,8 @@ def serve(config):
   if not os.path.exists(software_link):
     os.mkdir(software_link)
   slapos.runner.process.setHandler()
+  self.logger.info('Running slapgrid...')
   runInstanceWithLock(app.config)
+  self.logger.info('Done.')
   app.run(host=config.runner_host, port=int(config.runner_port),
       debug=config.debug, threaded=True)
