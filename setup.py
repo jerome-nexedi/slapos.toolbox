dbb92076ce006fddfe23b93b2df58dc40c0f5d2b
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-07-08 13:34:20 +0200

    Advertise development of new version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '0.35.0'
+version = '0.35.1-dev'
 name = 'slapos.toolbox'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
