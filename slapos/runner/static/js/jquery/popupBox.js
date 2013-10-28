54aa7f2505cbdb41c69ea1df16296e76989b5021
Alain Takoudjou <talino@tiolive.com>
2013-10-28 13:19:20 +0100

    Update popup box to display more than one msg at a time

--- slapos/runner/static/js/jquery/popupBox.js
+++ slapos/runner/static/js/jquery/popupBox.js
@@ -1,72 +1,65 @@
+/*jslint undef: true */
+/*global $, window, document */
+/* vim: set et sts=4: */
+
 // jQuery Message Popup
 // Display a message on the top of page, with floating
 //
 
 (function ($, document, window) {
-  var isShow = null;
-  var showDelayTimer = null;
+  var isShow = null,
+      top = 0,
+      boxCount = 0,
+      showDelayTimer = null;
+
   $.extend($.fn, {
     Popup: function(msg, option) {
-      var h;
+      var h,
+          $box = $(this),
+          currentBox = 'bcontent' + boxCount;
       if (option.type === undefined) option.type = "info";
       if (option.closebtn === undefined) option.closebtn = false;
       if (option.duration === undefined) option.duration = 0;
       if (option.load === undefined) option.load = false;
-      var $box = $(this);
-      if(showDelayTimer){clearTimeout(showDelayTimer);}
-      if(isShow){
-        $box.fadeOut('normal', function() {
-          setupBox();
-        });
-      }
-      else{setupBox();}
+
+      setupBox();
+
       function setupBox(){
         if (msg === undefined){
           msg = "Cannot execute your request. Please make sure you are logged in!!";
           option.type = "error";
         }
-        $box.empty();
-        $box.css('top','-1000px');
         $box.show();
-        $box.append('<div><table id="bcontent"><tr>' +
+        $box.css('top', + ($(window).scrollTop()) +'px');
+        $box.append('<div id="' + currentBox + '" style="display:none"><table><tr>' +
         '<td valign="middle" class="logo ' + option.type + '_message"></td>' +
         '<td valign="middle"><p>' + msg + '</p></td>' +
-        '<td valign="middle" class="b_close"><span id="pClose"></span></td></tr></table></div>');
+        '<td valign="middle" class="b_close"><span id="pClose'+boxCount+'" class="pClose"></span></td></tr></table></div>');
+
+        $("#pClose"+boxCount).bind("click", function() {
+          close($("#"+currentBox));
+        });
+        $("#"+currentBox).fadeIn();
+        boxCount++;
         $(window).scroll(function(){
           $box.animate({top:$(window).scrollTop()+"px" },{queue: false, duration: 350});
         });
-        h = $("#bcontent").height()+5;
-        $("#pClose").bind("click", function() {
-          close();
-        });
-        showBox();
         if(option.duration !== 0){
           showDelayTimer = setTimeout(function(){
             showDelayTimer = null;
-            close();
+            close($("#"+currentBox));
           }, option.duration);
         }
-      }
-      function showBox(){
         if(option.load){
           $(window).load(function(){
-            $box.css('top', + ($(window).scrollTop() - h) +'px');
-            $box.animate({ top:"+=" + h + "px" }, "slow");
-            isShow = true;
+
           });
-        }
-        else{
-          $box.css('top', + ($(window).scrollTop() - h) +'px');
-          $box.animate({ top:"+=" + h + "px" }, "slow");
-          isShow = true;
         }
       }
-      function close(){
-        $box.animate({ top:"-=" + h + "px" }, "slow", function(){
-          $box.fadeOut("normal", function() {
-            isShow = false;
-          });
-        });
+      function close($elt){
+        $elt.unbind('click');
+        $elt.fadeOut();
+        $elt.remove();
       }
     }
   });
